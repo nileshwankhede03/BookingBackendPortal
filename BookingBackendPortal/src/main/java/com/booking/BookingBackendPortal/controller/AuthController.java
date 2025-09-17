7e85846a3c0c6e6e41665a13bd2182e515238457
Nilesh Wankhede <114302591+nileshwankhede03@users.noreply.github.com>
2025-09-17 11:58:49 +0530

    First commit

---
+++ BookingBackendPortal/src/main/java/com/booking/BookingBackendPortal/controller/AuthController.java
@@ -0,0 +1,45 @@
+package com.booking.BookingBackendPortal.controller;
+
+import com.booking.BookingBackendPortal.dto.JwtResponse;
+import com.booking.BookingBackendPortal.dto.LoginRequest;
+import com.booking.BookingBackendPortal.entity.User;
+import com.booking.BookingBackendPortal.repository.UserRepository;
+import com.booking.BookingBackendPortal.service.AuthService;
+import com.booking.BookingBackendPortal.service.JwtService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+        String jwt = authService.authenticateUser(loginRequest.getUsername(), loginRequest.getPassword());
+
+        User user = userRepository.findByUsername(loginRequest.getUsername())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        List<String> roles = user.getRoles().stream()
+                .map(Enum::name)
+                .collect(Collectors.toList());
+        //.map(role -> role.name())
+
+        return ResponseEntity.ok(new JwtResponse(jwt, user.getId(), user.getUsername(), roles));
+    }
+}
